7fb1f395e98667043058f487ffefaff2392aa3f9
hao <qiuhao2007@gmail.com>
2020-01-26 00:23:00 +0100

    pas finir

--- data/image_folder.py
+++ data/image_folder.py
@@ -60,6 +60,7 @@ OK: images[] peut returner les fichers en dcm.
 '''
 def dicomImageOpen(path):
     ds=pydicom.dcmread(path)
+    ds.pixel_array=ds.pixel_array+2000 #transforme CT Hu de -2000~+3000 à 0 ~ 5000
     return Image.fromarray(ds.pixel_array).convert('I')
     
     '''array_buffer=ds.pixel_array.tobytes()
